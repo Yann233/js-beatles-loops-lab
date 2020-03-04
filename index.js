ad204a2dc6b675f0aaec0ec196018dc6aa5981b1
Yann Xiaoyan Yang <yannxyang@sina.com>
2020-03-04 19:51:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments){
+var arrayOfString = [];//Create a variable with an empty array
+for (let i = 0; i < musicians.length; i++) {
+  //loop这里var i=0也行
+ //returns an array of strings containing what instruments each musician plays
+arrayOfString.push(musicians[i] + " plays " + instruments[i])
+}
+return arrayOfString;
+}
+//////////////////////////////
+function johnLennonFacts(facts){
+  var arrayFact=[];
+  var i=0;
+  while(facts[i]){//这个i就是counter
+  arrayFact.push(facts[i++] + "!!!")//arrayFact.push(facts[i] + "!!!")
+                                    //i++;也可以这样
+}
+  return arrayFact;
+}
+/////////////////////
+function iLoveTheBeatles(number){
+  var emptyArray = [];
+  do {
+    emptyArray.push("I love the Beatles!")
+    number++;
+  }
+  while ( number < 15);
+  return emptyArray;
+}
